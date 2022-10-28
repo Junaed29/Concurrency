3e66aa2593effc44e0ed6cfa888888f8ea0bd4e8
Junaed <junaed1995@gmail.com>
2022-10-28 17:20:18 +0600

    * Synchronous Example

--- Concurrency/ViewController.swift
+++ Concurrency/ViewController.swift
@@ -11,9 +11,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let queue_1 = DispatchQueue(label: "first")
+        queue_1.sync {
+            sleep(4)
+            print("First queue exicuted")
+        }
+        
+        let queue_2 = DispatchQueue(label: "second")
+        queue_2.sync {
+            sleep(4)
+            print("Second queue exicuted")
+        }
+        
+        print("All Queue Exicuted")
     }
 
+    @IBAction func greenButtonPressed(_ sender: UIButton) {
+        
+//        let queue = DispatchQueue(label: "downloadimage")
+//        queue.async {
+//            sleep(5)
+//            DispatchQueue.main.async {
+//                self.view.backgroundColor = sender.backgroundColor
+//            }
+//        }
 
+
+    }
+
+    @IBAction func redButtonPressed(_ sender: UIButton) {
+        view.backgroundColor = sender.backgroundColor
+    }
 }
 
